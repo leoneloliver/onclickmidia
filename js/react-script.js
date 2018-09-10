9b65eebd3f00ff47a129a3010d552a3a1494299e
Leonel Oliveira - OnResearch Inc <loliveira@hq.onresearch.com>
2018-09-10 16:36:36 -0400

    updates

--- js/react-script.js
+++ js/react-script.js
@@ -49,7 +49,7 @@ var ComponentProject = React.createClass({
     )      
   }
 });
-React.render(<ComponentProject source="https://api.myjson.com/bins/9eqmg" />, document.querySelector("project-list"));
+React.render(<ComponentProject source="https://api.myjson.com/bins/l3nk0" />, document.querySelector("project-list"));
 
 
 var ComponentSkill = React.createClass({
